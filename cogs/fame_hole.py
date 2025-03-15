580fccc4a18db84f40e4bd808178a66b5fe7b622
DrSien <168520879+DrSien@users.noreply.github.com>
2025-03-15 16:24:47 +0500

    added new categories

--- cogs/fame_hole.py
+++ cogs/fame_hole.py
@@ -23,6 +23,9 @@ def new_rank_base(guild):
 "public"	INTEGER NOT NULL DEFAULT 0,
 "role active"	INTEGER NOT NULL DEFAULT 0,
 "role custom"	INTEGER NOT NULL DEFAULT 0,
+"fmafia victory"	INTEGER NOT NULL DEFAULT 0,
+"fmafia highlight"	INTEGER NOT NULL DEFAULT 0,
+"fmafia game"	INTEGER NOT NULL DEFAULT 0,
 "past seasons"	INTEGER NOT NULL DEFAULT 0
 )''')
     con.close
@@ -108,12 +111,27 @@ def OzRank(uid, guild, act, case, count):
             new = old+n if act == 1 else old-n
             con.execute('UPDATE rank SET "role custom" = {new} WHERE uid={uid}'.format(new = new, uid = uid))
             resp = 'за кастомку "просто так" **{old}** -> **{new}**'.format(old = old, new = new)
+        elif case == 9:
+            n = count if count != None else 2
+            new = old+n if act == 1 else old-n
+            con.execute('UPDATE rank SET "fmafia victory" = {new} WHERE uid={uid}'.format(new = new, uid = uid))
+            resp = 'за победу в фмафеи **{old}** -> **{new}**'.format(old = old, new = new)
+        elif case == 10:
+            n = count if count != None else 2
+            new = old+n if act == 1 else old-n
+            con.execute('UPDATE rank SET "fmafia highlight" = {new} WHERE uid={uid}'.format(new = new, uid = uid))
+            resp = 'Хайлайтер фмафеи **{old}** -> **{new}**'.format(old = old, new = new)
+        elif case == 11:
+            n = count if count != None else 2
+            new = old+n if act == 1 else old-n
+            con.execute('UPDATE rank SET "fmafia game" = {new} WHERE uid={uid}'.format(new = new, uid = uid))
+            resp = 'за проведение партии в фмафеи **{old}** -> **{new}**'.format(old = old, new = new)
         else:
             con.close()
             return False
         cursor = con.execute('SELECT * FROM rank WHERE uid = {}'.format(uid))
         r = cursor.fetchone()
-        summa = r[2]+r[3]+r[4]+r[5]+r[6]+r[7]+r[8]+r[9]
+        summa = r[2]+r[3]+r[4]+r[5]+r[6]+r[7]+r[8]+r[9]+r[10]+r[11]+r[12]
         con.execute('UPDATE rank SET "sum" = {summa} WHERE uid={uid}'.format(summa = summa, uid = uid))
         con.commit()
         con.close()
@@ -141,18 +159,21 @@ def member_score(user, interaction):
     else:
         rank_embed = discord.Embed(title='Баллы активности {}'.format(user.display_name), color=discord.Color.random())
         rank_embed.set_thumbnail(url = user.display_avatar)
-        rank_embed.add_field(name='Баллы в сезоне: {}'.format(exrank[1]), value='\
-* Победы в ивенте: {event}\n\
-* Победы в SIGame: {sigame}\n\
-* Топ-1 недели: {top1}\n\
-* Топ-3 недели: {top3}\n\
-* Посты в библиотеке: {lib}\n\
-* Посты в публикациях: {posts}\n\
-* Кастомки за заслуги: {roleactive}\n\
-* Кастомки "просто так": {rolejust}\n\
+        rank_embed.add_field(name=f'Баллы в сезоне: {exrank[1]}', value=f'\
+* Победы в ивенте: {exrank[2]}\n\
+* Победы в SIGame: {exrank[3]}\n\
+* Топ-1 недели: {exrank[4]}\n\
+* Топ-3 недели: {exrank[5]}\n\
+* Посты в библиотеке: {exrank[6]}\n\
+* Посты в публикациях: {exrank[7]}\n\
+* Кастомки за заслуги: {exrank[8]}\n\
+* Кастомки "просто так": {exrank[9]}\n\
+* Победа в фмафеи: {exrank[10]}\n\
+* Хайлайтер фмафеи: {exrank[11]}\n\
+* Проведение партии в фмафеи: {exrank[12]}\n\
 \n\
-*Баллы в прошлах сезонах: {pastseasons}*\n\
--# Всего баллов: {total}'.format(event = exrank[2], sigame = exrank[3] , top1 = exrank[4], top3 = exrank[5], lib = exrank[6], posts = exrank[7], roleactive = exrank[8], rolejust = exrank[9], pastseasons = exrank[10], total = exrank[1] + exrank[10]))
+*Баллы в прошлах сезонах: {exrank[13]}*\n\
+-# Всего баллов: {exrank[1] + exrank[13]}')
     return rank_embed
 ## TOPS
 # top 10
@@ -211,7 +232,7 @@ def create_report(interaction):
     report_path = "Отчёт зал славы {season} ({date}).csv".format(season=season, date=datetime.now().strftime("%d%m%Y%H%M%S"))
     
     with open(report_path, 'a', encoding='utf-8-sig') as tab:
-        tab.write('Участник;Ивенты;Сигеймы;Топ-1;Топ-3;Библиотека;Публикации;Роль за актив;Роль просто так;Сумма сезона;Прошлые сезоны')
+        tab.write('Участник;Ивенты;Сигеймы;Топ-1;Топ-3;Библиотека;Публикации;Роль за актив;Роль просто так;Победа в фмафеи;Хайлайтер фмафеи;Проведение партии в фмафеи;Сумма сезона;Прошлые сезоны')
         
         for row in cursor.execute('SELECT * FROM "rank" ORDER BY "sum" DESC').fetchall():
             user = interaction.guild.get_member(row[0])
@@ -220,7 +241,7 @@ def create_report(interaction):
             else:
                 user_display = f"Потерянный пользователь ({row[0]})"
             
-            tab.write(f"\n{user_display};{row[2]};{row[3]};{row[4]};{row[5]};{row[6]};{row[7]};{row[8]};{row[9]};{row[1]};{row[10]}")
+            tab.write(f"\n{user_display};{row[2]};{row[3]};{row[4]};{row[5]};{row[6]};{row[7]};{row[8]};{row[9]};{row[10]};{row[11]};{row[12]};{row[1]};{row[13]}")
     conn.close()
 
     return report_path
@@ -270,6 +291,9 @@ res = {
     6: 'за публикацию в "Публикациях"',    # DarkViolet (#9400D3)
     7: 'за кастомную роль за заслуги',  # OrangeRed (#FF4500)
     8: 'за кастомную роль "просто так"',    # Pink (#FFC0CB)
+    9: 'за победу в фмафеи',    # DarkSlateBlue (#483D8B)
+    10: 'Хайлайтер фмафеи', # SkyBlue (#87CEEB)
+    11: 'за проведение партии в фмафеи' # MidnightBlue (#191970)
 }
 col = {
     1: 0xFFD700,
@@ -279,7 +303,10 @@ col = {
     5: 0x0000CD,
     6: 0x9400D3,
     7: 0xFF4500,
-    8: 0xFFC0CB
+    8: 0xFFC0CB,
+    9: 0x483D8B,
+    10: 0x87CEEB,
+    11: 0x191970
 }
 
 def Fame_notifications(author, user, guild, action, reason, count):
@@ -343,14 +370,17 @@ class FameGroup(commands.GroupCog, name="активность", ):
             app_commands.Choice(name="Отнять", value=2),
         ],
         reason=[
-            app_commands.Choice(name="Победа в ивенте (6 баллов)", value=1),
-            app_commands.Choice(name="Победа в SIGame (4 балла)", value=2),
-            app_commands.Choice(name="Топ-1 по опыту в неделе (2 балла)", value=3),
-            app_commands.Choice(name="Топ-3 по опыту в неделе (1 балл)", value=4),
-            app_commands.Choice(name="Публикация в библиотеке (1 балл)", value=5),
-            app_commands.Choice(name="Публикация в публикации (2 балла)", value=6),
-            app_commands.Choice(name="Кастомка за актив или заслуги (3 балла)", value=7),
-            app_commands.Choice(name="Кастомка 'просто так' (2 балла)", value=8),
+            app_commands.Choice(name="Победа в ивенте - 6 баллов", value=1),
+            app_commands.Choice(name="Победа в SIGame - 4 балла", value=2),
+            app_commands.Choice(name="Топ-1 по опыту в неделе - 2 балла", value=3),
+            app_commands.Choice(name="Топ-3 по опыту в неделе - 1 балл", value=4),
+            app_commands.Choice(name="Публикация в библиотеке - 1 балл", value=5),
+            app_commands.Choice(name="Публикация в публикации - 2 балла", value=6),
+            app_commands.Choice(name="Кастомка за актив или заслуги - 3 балла", value=7),
+            app_commands.Choice(name="Кастомка 'просто так' - 2 балла", value=8),
+            app_commands.Choice(name="Победа в фмафеи - 2 балла  (3 балла за нейтрала)", value=9),
+            app_commands.Choice(name="Хайлайтер фмафеи - 1 балла", value=10),
+            app_commands.Choice(name="Проведение партии в фмафеи - 2 балла", value=11),
         ]
     )
     async def mod_score(
@@ -366,7 +396,7 @@ class FameGroup(commands.GroupCog, name="активность", ):
                 await interaction.response.send_message('У Вас нет доступа к этой команде', ephemeral=True)
                 return
             
-            default_scores = {1: 6, 2: 4, 3: 2, 4: 1, 5: 1, 6: 2, 7: 3, 8: 2}
+            default_scores = {1: 6, 2: 4, 3: 2, 4: 1, 5: 1, 6: 2, 7: 3, 8: 2, 9: 2, 10: 1, 11: 2}
             count = count or default_scores.get(reason.value, 0)
             
             exrank = OzRank(user.id, interaction.guild, action.value, reason.value, count)
@@ -439,7 +469,7 @@ class FameGroup(commands.GroupCog, name="активность", ):
         if is_gm(interaction.user) == True:
             con = sqlite3.connect('base {}.db'.format(interaction.guild.id))
             # recount
-            con.execute('UPDATE rank SET "sum" = "event"+"sigame"+"top 1"+"top 3"+"lib"+"public"+"role active"+"role custom"')
+            con.execute('UPDATE rank SET "sum" = "event"+"sigame"+"top 1"+"top 3"+"lib"+"public"+"role active"+"role custom"+"fmafia victory"+"fmafia highlight"+"fmafia game"')
             con.commit()
             ready_reset = True
             # report
@@ -450,7 +480,7 @@ class FameGroup(commands.GroupCog, name="активность", ):
             await asyncio.sleep(300)
             if ready_reset == True:
                 con.execute('UPDATE rank SET "past seasons"="past seasons"+"sum"')
-                con.execute('UPDATE rank SET "sum" = 0, "event" = 0, "sigame" = 0, "top 1" = 0, "top 3" = 0, "lib" = 0, "public" = 0, "role active" = 0, "role custom" = 0')
+                con.execute('UPDATE rank SET "sum" = 0, "event" = 0, "sigame" = 0, "top 1" = 0, "top 3" = 0, "lib" = 0, "public" = 0, "role active" = 0, "role custom" = 0, "fmafia victory" = 0, "fmafia highlight" = 0, "fmafia game" = 0')
                 con.execute('UPDATE "glory settings" SET "season" = "season"+1')
                 con.commit()
         else:
