a264e572141521c1597c41f38ffa9a1c4f00e085
tbmpc <tbm@Panoptic.local>
2025-09-27 21:10:54 +0300

    hotfix: everyone can now see voice channels

--- cogs/voicemanager.py
+++ cogs/voicemanager.py
@@ -74,8 +74,8 @@ class VoiceManager(commands.Cog):
         # - forbidden roles: explicitly denied view and connect
         overwrites: dict[discord.abc.Snowflake, discord.PermissionOverwrite] = {}
 
-        # Default: deny everyone from seeing/connecting (we'll allow owner & bot explicitly)
-        overwrites[guild.default_role] = discord.PermissionOverwrite(view_channel=False, connect=False)
+        # Default: deny everyone from connecting (we'll allow owner & bot explicitly)
+        overwrites[guild.default_role] = discord.PermissionOverwrite(view_channel=True, connect=False)
 
         # Deny connect/view for configured forbidden roles (if they exist in guild)
         for rid in FORBIDDEN_ROLE_IDS:
