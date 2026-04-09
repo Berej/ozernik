adcfccd260874c42178ae2bd024620859cd3f37b
Gafasad-gab <gadriel2647@gmail.com>
2026-04-10 00:23:39 +0500

    Created a karma system module

---
+++ modules/karma_sistem/karma_sistem.py
@@ -0,0 +1,13 @@
+import asyncio
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord import Embed
+from config_loader import config
+
+class KarmaSistem(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(KarmaSistem(bot))
